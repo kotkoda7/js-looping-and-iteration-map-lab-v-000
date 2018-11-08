764d8f07d8583e6bcbd5e34f660d86393c093733
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-11-08 19:14:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ function nameToAttributes(list) {
   return list.map(function(driver) {
     const driverFirstName = driver.split(" ")[0];
     const driverLastName = driver.split(" ")[1];
+    
+    return { firstName: driverFirstName, lastName: driverLastName };
   }
   )}
   
   function attributesToPhrase(drivers) {
     return drivers.map(function(driver) {
     return ${driver.name} is from ${driver.hometown};
-  )}
+  )};
   }
