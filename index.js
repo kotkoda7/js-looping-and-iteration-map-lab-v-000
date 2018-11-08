a2b4dfceded43fc2e9d2845393e1c421fd8b9461
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-11-08 19:13:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function nameToAttributes(list) {
   
   function attributesToPhrase(drivers) {
     return drivers.map(function(driver) {
-    
+    return ${driver.name} is from ${driver.hometown};
   )}
   }
