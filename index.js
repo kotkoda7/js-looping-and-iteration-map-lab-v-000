73757845e5854a27bb0e25744e55e846142a576c
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-11-08 19:08:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file.
 function lowerCaseDrivers(list) {
-  return list.map(function(driver)) {
-    return driver.toLowerCase
-  }
+  return list.map(function(driver) {
+    return driver.toLowerCase;
+  });
 }
 
 function nameToAttributes()
